b7c290e28233107629295aa72be753b85053fa62
Mateusz <matpia98@gmail.com>
2024-08-11 19:03:43 +0200

    continued integration tests

--- src/integration/java/feature/HappyPathIntegrationTest.java
+++ src/integration/java/feature/HappyPathIntegrationTest.java
@@ -1,8 +1,11 @@
 package feature;
 
 import com.example.expensetracking.ExpenseTrackingApplication;
+import com.example.expensetracking.domain.crud.dto.ExpenseResponseDto;
 import com.example.expensetracking.domain.reporting.dto.ReportDto;
 import com.example.expensetracking.infrastructure.crud.controller.category.dto.GetAllCategoriesResponseDto;
+import com.example.expensetracking.infrastructure.crud.controller.category.dto.GetExpensesByCategoryResponseDto;
+import com.example.expensetracking.infrastructure.crud.controller.expense.dto.GetAllExpensesResponseDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -98,7 +101,7 @@ class HappyPathIntegrationTest {
         // step 5. when I go to /reports/custom, I can see a report with no expenses
         // given & when
         ReportDto initialCustomReport = webTestClient.get().uri("/reports/custom?startDate={startDate}&endDate={endDate}",
-                LocalDate.now().minusDays(7), LocalDate.now())
+                        LocalDate.now().minusDays(7), LocalDate.now())
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(ReportDto.class)
@@ -115,13 +118,13 @@ class HappyPathIntegrationTest {
         webTestClient.post().uri("/expenses")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue("""
-                    {
-                        "title": "Groceries",
-                        "description": "Weekly groceries",
-                        "amount": 100.0,
-                        "categoryId": 1
-                    }
-                    """)
+                        {
+                            "title": "Groceries",
+                            "description": "Weekly groceries",
+                            "amount": 100.0,
+                            "categoryId": 1
+                        }
+                        """)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
@@ -132,6 +135,99 @@ class HappyPathIntegrationTest {
                 .jsonPath("$.date").isNotEmpty()
                 .jsonPath("$.categoryId").isEqualTo(1)
                 .jsonPath("$.categoryName").isEqualTo("Groceries");
+
+        // step 7. when I go to /categories/5, I can see the category Healthcare with id 5
+        // given & when & then
+        webTestClient.get().uri("/categories/5")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(5)
+                .jsonPath("$.name").isEqualTo("Healthcare");
+
+        // step 8: when I post to /expenses, I can see new expense with id 2
+        webTestClient.post().uri("/expenses")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                            "title": "Dinner",
+                            "description": "Restaurant meal",
+                            "amount": 50.0,
+                            "categoryId": 7
+                        }
+                        """)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(2)
+                .jsonPath("$.title").isEqualTo("Dinner")
+                .jsonPath("$.description").isEqualTo("Restaurant meal")
+                .jsonPath("$.amount").isEqualTo(50.0)
+                .jsonPath("$.date").isNotEmpty()
+                .jsonPath("$.categoryId").isEqualTo(7)
+                .jsonPath("$.categoryName").isEqualTo("Dining Out");
+
+        // step 9: when I go post to /expenses, I can see new expense with id 3
+        webTestClient.post().uri("/expenses")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                            "title": "Breakfast",
+                            "description": "Fast breakfast in a cafe",
+                            "amount": 15.0,
+                            "categoryId": 7
+                        }
+                        """)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.id").isEqualTo(3)
+                .jsonPath("$.title").isEqualTo("Breakfast")
+                .jsonPath("$.description").isEqualTo("Fast breakfast in a cafe")
+                .jsonPath("$.amount").isEqualTo(15.0)
+                .jsonPath("$.date").isNotEmpty()
+                .jsonPath("$.categoryId").isEqualTo(7)
+                .jsonPath("$.categoryName").isEqualTo("Dining Out");
+
+        // step 10: when I go to /expenses, I can see 3 expenses
+        // given & when
+        GetAllExpensesResponseDto getThreeExpensesResponseDto = webTestClient.get()
+                .uri("/expenses")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(GetAllExpensesResponseDto.class)
+                .returnResult()
+                .getResponseBody();
+        // then
+        assertThat(getThreeExpensesResponseDto).isNotNull();
+        assertThat(getThreeExpensesResponseDto.expenses()).hasSize(3);
+        assertThat(getThreeExpensesResponseDto.expenses())
+                .extracting(ExpenseResponseDto::id)
+                .containsExactly(1L, 2L, 3L);
+        assertThat(getThreeExpensesResponseDto.expenses())
+                .extracting(ExpenseResponseDto::title)
+                .containsExactly("Groceries", "Dinner", "Breakfast");
+
+        // step 11: when I go to /categories/7/expenses, I can see 2 expenses
+        // given & when
+        GetExpensesByCategoryResponseDto getTwoExpensesByCategoryResponseDto = webTestClient.get()
+                .uri("/categories/7/expenses")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(GetExpensesByCategoryResponseDto.class)
+                .returnResult()
+                .getResponseBody();
+
+        // then
+        assertThat(getTwoExpensesByCategoryResponseDto).isNotNull();
+        assertThat(getTwoExpensesByCategoryResponseDto.expenses()).hasSize(2);
+        assertThat(getTwoExpensesByCategoryResponseDto.expenses())
+                .extracting(ExpenseResponseDto::id)
+                .containsExactly(2L, 3L);
+        assertThat(getTwoExpensesByCategoryResponseDto.expenses())
+                .extracting(ExpenseResponseDto::title)
+                .containsExactly("Dinner", "Breakfast");
+
 
 
     }
